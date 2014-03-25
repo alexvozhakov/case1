df4dafb4cfc8c39711ba8e07e8bbfe0f68fee3a6
Artem <Hezhay@mail.ru>
2014-03-25 13:45:15 +0700

    Add Fabric Class

---
+++ Case1/Case1/BinaryCalculators/FabricClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case1.BinaryCalculators
+{
+    public static class FabricClass
+    {
+        public static IBinaryCalculation CreateOperation(string name)
+        {
+            switch (name)
+            {
+                case "Adder":
+                    return new Adder();
+                case "Deduction":
+                    return new Deduction();
+                case "Mult":
+                    return new Multiplication();
+                case "Sub":
+                    return new Substraction();
+                default:
+                    throw new ArgumentException("Unknown argument", "name");
+            }
+        }
+    }
+}
