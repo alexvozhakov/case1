18461aba0610623d2a7112e5c490e93b07458534
Savinykh <ubermensch.rk@gmail.com>
2014-04-15 14:23:36 +0700

    Finish my part of Tests.

---
+++ Case1/Case1.Test/FirstCalculations/LnTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Case1.FirstCalculations;
+using NUnit.Framework;
+
+namespace Case1.Test.FirstCalculations
+{
+    [TestFixture]
+    public class LnTest
+    {
+        [Test]
+        public void LnatTest()
+        {
+            IOneCalculation calculater = FactoryClass2.CreateOperation("ln");
+            double result = calculater.Calculate(10);
+            Assert.AreEqual(2.3025, result, 0.0001);
+        }
+    }
+}
