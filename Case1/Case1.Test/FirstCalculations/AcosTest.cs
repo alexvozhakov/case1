286bee5635451039486a17c88eeef7edba7e9488
AV <ubermensch.rk@gmail.com>
2014-04-15 14:35:28 +0700

    add tests acos asin

---
+++ Case1/Case1.Test/FirstCalculations/AcosTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Case1.FirstCalculations;
+using NUnit.Framework;
+
+namespace Case1.Test.FirstCalculations
+{
+    [TestFixture]
+    public class AcosTest
+    {
+        [Test]
+        public void AcosinusTest()
+        {
+            IOneCalculation calculater = FactoryClass2.CreateOperation("Acos");
+            double result = calculater.Calculate(1);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
