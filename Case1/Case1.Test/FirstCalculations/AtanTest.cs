49c5104b2c075e43641c6d1647364d941ebb1255
AV <ubermensch.rk@gmail.com>
2014-04-15 14:42:28 +0700

    add tests log10

---
+++ Case1/Case1.Test/FirstCalculations/AtanTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Case1.FirstCalculations;
+using NUnit.Framework;
+
+namespace Case1.Test.FirstCalculations
+{
+    [TestFixture]
+    public class AtanTest
+    {
+        [Test]
+        public void AtangTest()
+        {
+            IOneCalculation calculater = FactoryClass2.CreateOperation("Atan");
+            double result = calculater.Calculate(1);
+            Assert.AreEqual(0.7853, result, 0.0001);
+        }
+    }
+}
---
+++ Case1/Case1.Test/FirstCalculations/Log10Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Case1.FirstCalculations;
+using NUnit.Framework;
+
+namespace Case1.Test.FirstCalculations
+{
+    [TestFixture]
+    public class Log10Test
+    {
+        [Test]
+        public void Logar10Test()
+        {
+            IOneCalculation calculater = FactoryClass2.CreateOperation("log10");
+            double result = calculater.Calculate(25);
+            Assert.AreEqual(1.3979, result, 0.0001);
+        }
+    }
+}
