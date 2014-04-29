0e465224dac8812fdfad739de6b6160c89d349f7
AV <HEZHAY@mail.ru>
2014-04-29 13:48:37 +0700

    Add Test Bubble

---
+++ Case1/Case1.Test/MassSort/BubbleTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Case1.MassSort;
+using NUnit.Framework;
+
+namespace Case1.Test.MassSort
+{
+    [TestFixture]
+    class BubbleTest
+    {
+        [Test]
+        public void BubTest()
+        {
+            ISort calculater = FactorySort.CreateOperation("Bubble");
+            int[] result = calculater.SortMass(new []{5, 6, 3, 2, 9, 10, 12});
+            int[] expected = new[] {2, 3, 5, 6, 9, 10, 12}; 
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
